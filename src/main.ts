75ad872f35b06828482f04cebdf64f889378d713
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-16 23:53:40 +0600

    add: config module and env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const port = configService.get<number>('port') || 3000;
+  await app.listen(port);
 }
 bootstrap();
