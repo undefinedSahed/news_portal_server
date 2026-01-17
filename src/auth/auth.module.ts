bc08ea07003043b443e94fa5965e0e0ffcccedca
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-17 10:47:41 +0600

    fix: jwt token generation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     JwtModule.registerAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('jwt.secret'),
-        signOptions: {
-          expiresIn: Number(configService.get<string>('jwt.expiresIn')),
-        },
-      }),
+      useFactory: (configService: ConfigService) =>
+        ({
+          secret: configService.get<string>('jwt.secret'),
+          signOptions: {
+            expiresIn: configService.get<string>('jwt.expiresIn'),
+          },
+        }) as JwtModuleOptions,
     }),
   ],
   controllers: [AuthController],
