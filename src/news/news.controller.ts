c4f2c422f2aeaf3f93306d0347fd7fe69a3cee0a
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-01-17 11:32:44 +0600

    add: create news route with no image

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { NewsService } from './news.service';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { CreateNewsDto } from './dto/create-news.dto';
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {}
+
+  // Create news
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(@Body() createNewsDto: CreateNewsDto) {
+    return this.newsService.create(createNewsDto);
+  }
+}
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { News } from './schemas/news.schema';
+import { Model } from 'mongoose';
+import { CreateNewsDto } from './dto/create-news.dto';
+import slugify from 'slugify';
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+  constructor(
+    @InjectModel(News.name)
+    private readonly newsModel: Model<News>,
+  ) {}
+
+  async create(createNewsDto: CreateNewsDto) {
+    const slug = slugify(createNewsDto.title, { lower: true });
+
+    const existingNews = await this.newsModel.findOne({ slug });
+
+    if (existingNews) {
+      throw new ConflictException('News with this title already exists');
+    }
+
+    return this.newsModel.create({
+      ...createNewsDto,
+      slug,
+    });
+  }
+}
